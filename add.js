3900994549b03f51d9b4ae70a9751799d10bbe1e
vanessa <vanessa.chaillou@orange.fr>
2017-12-05 14:10:24 +0100

    fin

---
+++ add.js
@@ -0,0 +1,36 @@
+var a = Number(process.argv[2]);
+var b = Number(process.argv[3]);
+
+function addition(a , b){
+	return  a + b;	
+}
+console.log(addition(a,b));
+
+
+
+
+var a = Number(process.argv[2]);
+var b = Number(process.argv[3]);
+
+function substract(a , b){
+	return  a - b;	
+}
+console.log(substract(a,b));
+
+
+ar a = Number(process.argv[2]);
+var b = Number(process.argv[3]);
+
+function multiply(a , b){
+	return  a * b;	
+}
+console.log(multiply(a,b));
+
+
+ar a = Number(process.argv[2]);
+var b = Number(process.argv[3]);
+
+function divide(a , b){
+	return  a / b;	
+}
+console.log(divide(a,b));
